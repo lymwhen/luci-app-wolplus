b2f025a413852b20a919da3dde97cfc562889564
haiyiya <1074325187@qq.com>
2025-11-20 23:32:59 +0800

    更新语言文件

--- luasrc/model/cbi/wolplus.lua
+++ luasrc/model/cbi/wolplus.lua
@@ -1,25 +1,25 @@
 local i = require "luci.sys"
 local t, e
-t = Map("wolplus", translate("Wake on LAN +"), translate("Wake on LAN is a mechanism to remotely boot computers in the local network. ") .. [[<a href="https://github.com/sundaqiang/openwrt-packages" target="_blank">Powered by sundaqiang</a>]])
+t = Map("wolplus", translate("wolplus"), [[<a href="https://github.com/sundaqiang/openwrt-packages" target="_blank">Powered by sundaqiang</a>]])
 t.template = "wolplus/index"
-e = t:section(TypedSection, "macclient", translate("Host Clients"))
+e = t:section(TypedSection, "macclient", translate("macclient"))
 e.template = "cbi/tblsection"
 e.anonymous = true
 e.addremove = true
 ---- add device section
-a = e:option(Value, "name", translate("Name"))
+a = e:option(Value, "name", translate("name"))
 a.optional = false
 ---- mac address
-nolimit_mac = e:option(Value, "macaddr", translate("MAC Address"))
+nolimit_mac = e:option(Value, "macaddr", translate("macaddr"))
 nolimit_mac.rmempty = false
 i.net.mac_hints(function(e, t) nolimit_mac:value(e, "%s (%s)" % {e, t}) end)
 ----- network interface
-nolimit_eth = e:option(Value, "maceth", translate("Network Interface"))
+nolimit_eth = e:option(Value, "maceth", translate("maceth"))
 nolimit_eth.rmempty = false
 for t, e in ipairs(i.net.devices()) do if e ~= "lo" then nolimit_eth:value(e) end end
 ----- wake device
-btn = e:option(Button, "_awake",translate("Wake Up Host"))
-btn.inputtitle	= translate("Awake")
+btn = e:option(Button, "_awake", translate("operation"))
+btn.inputtitle	= translate("awake")
 btn.inputstyle	= "apply"
 btn.disabled	= false
 btn.template = "wolplus/awake"
